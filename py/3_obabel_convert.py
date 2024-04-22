bd48a2263ce45b07286e06b1d973663fb2e117e1
Jeremy Gareth Burgess <133377607+JGBurgess1@users.noreply.github.com>
2024-04-22 14:44:30 +0200

    openbabel_script

---
+++ py/3_obabel_convert.py
@@ -0,0 +1,27 @@
+import argparse
+import os
+import subprocess
+
+parser = argparse.ArgumentParser(description="Converts pdb files to pdbqt files via openbabel.")
+parser.add_argument("-r", "--rank", required=True, help="The rank of the mpi process")
+args = parser.parse_args()
+rank = int(args.rank)
+
+def convert():
+    size = 240
+    
+    arr = os.listdir("1_mutate_minimized")
+    
+    num_mutations = len(arr)
+    chunk_size = num_mutations//size
+    remainder = num_mutations%size
+
+    start_index = rank*chunk_size + min(rank, remainder)
+    end_index = start_index + chunk_size + (1 if rank < remainder else 0)
+
+    for file in arr[start_index:end_index]:
+        new_name = f"{file}qt"
+        subprocess.run(['obabel',file,'-O',new_name])
+
+if __name__ == "__main__":
+    convert()
